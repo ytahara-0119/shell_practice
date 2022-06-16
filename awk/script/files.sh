c43a6a91cc4a57445ed4dac2ac6bb4ccf2c3fd25
田原芳信 <taharayoshinobu@MacBook-Air-2.local>
2022-06-17 06:07:55 +0900

    YT add files of awk

---
+++ awk/script/files.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# ./files.sh [-l location] [--location location [-e extension [-- extension] [-h] [--help] [-s] [--stats]
+
+function usage() {
+	echo "USAGE: $0 [-l location] [--location location [-e extension [-- extension] [-h] [--help] [-s] [--stats]"
+	echo "Examples:"
+	echo "$0 -l /etc/ -e txt -s"
+	echo "$0 -e img --stats"
+	echo
+	exit 1
+}
+
+while [ $# -gt 0 ]
+do
+	case $1 in
+		-l|--location )
+			LOCATION="$2"
+			if ! [ -d "$LOCATION" ]; then
+				usage
+			fi
+			LOC_SET=1
+			shift
+			shift
+			;;
+		
+		-e|--extension )
+			EXT="$2"
+			shift
+			shift
+			;;
+		-s|--stats )
+			STATS=1
+			shift
+			;;
+		
+		-h|--help )
+			shift
+			usage
+			;;
+		
+		* )
+			usage
+			;;
+	esac
+
+done
+
