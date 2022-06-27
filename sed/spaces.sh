37b90c0ff1f8d4247e9eebd27abafb0055ccf243
田原芳信 <taharayoshinobu@MacBook-Air-2.local>
2022-06-28 05:47:48 +0900

    YT modified section 10-89

--- sed/spaces.sh
+++ sed/spaces.sh
@@ -6,6 +6,8 @@ function usage() {
 	exit 1
 }
 
+FIX=0 #0-do not perform fix 1-perform fix
+
 if [ $# -eq 0 ]; then
 	usage
 fi
@@ -14,6 +16,7 @@ while [ $# -gt 0 ]
 do
 	case "$1" in
 		-f|--fix )
+			FIX=1
 			shift
 			;;
 
@@ -33,3 +36,11 @@ do
 	esac
 
 done
+
+if [ $FIX -eq 1 ] && [ -f "$FILE" ]; then 
+	echo "Fixing spaces and tabs at the beginning and at the end of lines"
+	sed -i 's/[[:blank:]]\+$//' "$FILE"
+	sed -i 's/^[[:blank:]]\+//' "$FILE"
+	echo "DONE"
+
+fi
